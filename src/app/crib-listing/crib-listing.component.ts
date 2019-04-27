48c8928bea1a7bb29eaf0beccadd826740156693
pierrelambert41 <pierrelambert41@gmail.com>
2019-04-27 17:38:24 +0200

    adding data

--- src/app/crib-listing/crib-listing.component.ts
+++ src/app/crib-listing/crib-listing.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { cribs } from './../data/cribs';
 
-@Component({
+@Component ({
   selector: 'app-crib-listing',
   templateUrl: './crib-listing.component.html',
   styleUrls: ['./crib-listing.component.css']
 })
 export class CribListingComponent implements OnInit {
+
+  cribs: Array<any> = cribs;
 
   constructor() { }
 
