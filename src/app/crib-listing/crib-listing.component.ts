79d1d3d56059d37d0e8f0793bd2f3e2256d43040
pierrelambert41 <pierrelambert41@gmail.com>
2019-05-01 21:50:11 +0200

    adding pipe & sort algorithm

--- src/app/crib-listing/crib-listing.component.ts
+++ src/app/crib-listing/crib-listing.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { CribsService } from './../services/cribs.service';
+import { UtilService } from './../services/util.service';
 
 @Component ({
   selector: 'app-crib-listing',
@@ -12,10 +13,22 @@ export class CribListingComponent implements OnInit {
 
   cribs: Array<any>;
   error: string;
+  sortField: string = 'price';
+  sortDirection: string = 'asc';
+  sortFields: Array<string> = [
+    'address',
+    'area',
+    'bathrooms',
+    'bedrooms',
+    'price',
+    'type'
+  ];
 
   constructor(
     private http: HttpClient,
-    private cribsService: CribsService) { }
+    private cribsService: CribsService,
+    private utilService: UtilService
+    ) { }
 
   ngOnInit() {
 
---
+++ src/app/services/util.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilService {
+
+  constructor() { }
+
+  public capitalize(word: string): string {
+    return word.charAt(0).toUpperCase() + word.substring(1);
+  }
+}
